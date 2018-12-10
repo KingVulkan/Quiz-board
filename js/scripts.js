356f2c9ff62bf6cd7988c48b6f446b612c38200c
KingVulkan <manowfelow@gmail.com>
2018-12-10 11:17:33 +0300

    adds functions for javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
 function checkAns(){
-  var quesOne=document.querySelector('input[name="Question1"]:checked').val();
-  var quesTwo=document.querySelector('input[name="Question2"]:checked').val();
-  var quesThree=document.querySelector('input[name="Question3"]:checked').val();
-  var quesFour=document.querySelector('input[name="Question4"]:checked').val();
-  var quesFive=document.querySelector('input[name="Question5"]:checked').val();
+  var quesOne=document.querySelector('input[name="question1"]:checked').value;
+  var quesTwo=document.querySelector('input[name="question2"]:checked').value;
+  var quesThree=document.querySelector('input[name="question3"]:checked').value;
+  var quesFour=document.querySelector('input[name="question4"]:checked').value;
+  var quesFive=document.querySelector('input[name="question5"]:checked').value;
+  var correctScore=0;
+
+  if (quesOne=="b"){
+    correctScore+=20
+  }
+  if (quesTwo=="c"){
+    correctScore+=20
+  }
+  if (quesThree=="b"){
+    correctScore+=20
+  }
+  if (quesFour=="c"){
+    correctScore+=20
+  }
+  if (quesFive=="a"){
+    correctScore+=20
+  }
+  alert(correctScore);
 }
