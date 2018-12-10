ebfe6f15574ca4b2ae6cb945f38f173ebdfc3bf6
KingVulkan <manowfelow@gmail.com>
2018-12-10 10:52:55 +0300

    checks out input for the user

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,7 @@
-var quesOne = $("input[name='Question1']:checked").val();
-var quesTwo = $("input[name='Question1']:checked").val();
+function checkAns(){
+  var quesOne=document.querySelector('input[name="Question1"]:checked').val();
+  var quesTwo=document.querySelector('input[name="Question2"]:checked').val();
+  var quesThree=document.querySelector('input[name="Question3"]:checked').val();
+  var quesFour=document.querySelector('input[name="Question4"]:checked').val();
+  var quesFive=document.querySelector('input[name="Question5"]:checked').val();
+}
